07bbe5290ab3a07920a7dbfb2dc0e963e3ed112d
isaacy2012 <isaacy2012@gmail.com>
2021-10-20 14:19:49 +1300

    Added GradeTab stub

---
+++ src/ui/output/GradeTab.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { H3 } from "./H3";
+import {Assignment} from "../../model/Assignment";
+
+
+export default function GradeTab(props: {assignments: Assignment[]}) {
+    return(
+        <H3>Coming Soon!</H3>
+    );
+}
