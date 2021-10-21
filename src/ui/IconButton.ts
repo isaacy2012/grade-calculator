6d2fbe6e9a2ab5708c6dd46a19d57ae34c071cac
isaacy2012 <isaacy2012@gmail.com>
2021-10-21 17:10:45 +1300

    Create IconButton.ts

---
+++ src/ui/IconButton.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const IconButton = styled.button`
+  font-size: 1.5em;
+  background: none;
+  border: none;
+
+  &:hover {
+    font-size: 1.3em;
+  }
+`
+
+export const FixedIconButton = styled(IconButton)`
+  padding-right: 0;
+  width: 2.5rem;
+`
+
+
